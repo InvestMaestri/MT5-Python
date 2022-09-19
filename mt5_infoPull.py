7266fe208ac6173b363e868de9df9423e07c4a81
Maestri Investment Group Ltd <112180859+InvestMaestri@users.noreply.github.com>
2022-09-19 00:01:14 -0300

    Add updated information and formatting

--- mt5_infoPull.py
+++ mt5_infoPull.py
@@ -1,40 +1,41 @@
-#Script that imports Terminal, Account and Instrument data from a MT5 Terminal
-#By Eduardo Bogosian - 2022
+# Script that imports Terminal, Account and Instrument data from a MT5 Terminal
+# Made for article on Medium: https://medium.com/@eduardo-bogosian/how-to-pull-data-from-metatrader-5-with-python-4889bd92f62d
+# By Eduardo Bogosian - 2022
 
 import pandas as pd
 import MetaTrader5 as mt5
 
-#Starts connection with the terminal based on synthax
+# Starts connection with the terminal based on syntax
 mt5.initialize()
 
-#Terminal Information
+# Terminal Information
 terminal_info = mt5.terminal_info()._asdict()
-terminal_info_df = pd.DataFrame(list(terminal_info.items()),columns=['Property','Value'])
-print("Terminal Info:\n",terminal_info_df)
+terminal_info_df = pd.DataFrame(list(terminal_info.items()), columns=['Property', 'Value'])
+print("Terminal Info:\n", terminal_info_df)
 
-#Retrive if trade is allowed from dataframe matrix.
-trade_allowed_bool = terminal_info_df.iat[4,1]
+# Retrive if trade is allowed from dataframe matrix.
+trade_allowed_bool = terminal_info_df.iat[4, 1]
 
-#Check if trades permited
-if trade_allowed_bool == False:
+# Check if trades permited
+if not trade_allowed_bool:
     print("\nTrades are not allowed.")
-elif trade_allowed_bool == True:
+elif trade_allowed_bool:
     print("\nTrades permited.")
 else:
     print("\nUnkown trade status.")
 
-#Account information
+# Account information
 account_info = mt5.account_info()._asdict()
-account_info_df = pd.DataFrame(list(account_info.items()),columns=['Property','Value'])
-print("\nAccount Info:\n",account_info_df)
+account_info_df = pd.DataFrame(list(account_info.items()), columns=['Property', 'Value'])
+print("\nAccount Info:\n", account_info_df)
 
-#Instrument Information
+# Instrument Information
 instrument = 'AAPL'
 instrument_info = mt5.symbol_info(instrument)._asdict()
-instrument_info_df = pd.DataFrame(list(instrument_info.items()),columns=['Property','Value'])
+instrument_info_df = pd.DataFrame(list(instrument_info.items()), columns=['Property', 'Value'])
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print("\nData for:",instrument)
+    print("\nData for:", instrument)
     print(instrument_info_df)
-    
-#End of interactions with the terimanl
+
+# End of interactions with the terimanl
 mt5.shutdown()
