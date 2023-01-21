91ddd024cd5590cf068bf19446fb3222e8e6bdfc
Maestri Investment Group Ltd <112180859+InvestMaestri@users.noreply.github.com>
2023-01-21 09:11:42 -0300

    will add the trade direction on graph
    This commit will add trade direction next to the ticker name on the graph.

--- mt5_OpenTrades_Rotation.py
+++ mt5_OpenTrades_Rotation.py
@@ -2,6 +2,7 @@
 # Made for article on Medium: https://medium.com/@eduardo-bogosian/a-python-script-that-rotates-through-your-open-trades-in-mt5-2ccd9e7136f5
 # By Eduardo Bogosian - 2023
 
+# Imports
 import pytz
 import time
 import datetime
@@ -25,7 +26,7 @@ def get_today():
     current_month = int(current_date[5] + current_date[6])
     current_day = int(current_date[8] + current_date[9])
     today = datetime(current_year, current_month, current_day, tzinfo=timezone)
-    
+
     return today
 
 
@@ -53,7 +54,6 @@ def get_stop_loss(trades_df, instrument):
     stop_loss_loc = trades_df[trades_df['symbol'] == instrument].index.values
     stop_loss = trades_df.iloc[stop_loss_loc]['sl']
     stop_loss = float(stop_loss)
-    
     return stop_loss
 
 
@@ -65,20 +65,40 @@ def get_take_profit(trades_df, instrument):
     return take_profit
 
 
+def get_trade_direction(trades_df, instrument):
+    trade_direction_loc = trades_df[trades_df['symbol'] == instrument].index.values
+    trade_direction_indicator = trades_df.iloc[trade_direction_loc]['type']
+    trade_direction_indicator = str(trade_direction_indicator)
+    trade_direction = int(trade_direction_indicator[5])
+    if trade_direction == 0:
+        return 'Long'
+    elif trade_direction == 1:
+        return 'Short'
+    else:
+        return 'Unknown'
+
+
 def show_graph(trades_df, instrument_history, ticker, duration):
     open_price = get_open_price(trades_df, ticker)
     stop_loss = get_stop_loss(trades_df, ticker)
     take_profit = get_take_profit(trades_df, ticker)
-    plt.figure(figsize=(12, 6))
-    sns.lineplot(data=instrument_history, x=instrument_history['time'], y=instrument_history['close'], color='black')
+    trade_direction = str(get_trade_direction(trades_df, ticker))
+    symbol = str(ticker)
+    plt.figure(figsize=(16, 6))
+    sns.lineplot(data=instrument_history,
+                 x=instrument_history['time'],
+                 y=instrument_history['close'],
+                 color='black')
     plt.axhline(y=open_price, color='blue')
     plt.axhline(y=stop_loss, color='red')
     plt.axhline(y=take_profit, color='green')
-    plt.title(ticker)
+    plt.title(f'{symbol} - {trade_direction}')
     plt.show(block=False)
     plt.pause(1.00)
     time.sleep(duration)
     plt.close()
+
+# ------------------------------------------------
 
 
 def main():
@@ -89,12 +109,18 @@ def main():
         if not trades:
             print("No open trades. Waiting...")
             time.sleep(60)
-            break
+            continue
         else:
             open_trades = pd.DataFrame(list(trades),
                                        columns=trades[0]._asdict().keys())
             open_trades['time'] = pd.to_datetime(open_trades['time'],
                                                  unit='s')
+            with pd.option_context('display.max_rows',
+                                   None,
+                                   'display.max_columns',
+                                   None):
+                # print(open_trades)
+                pass
 
             # Get graph data for instrument[0]
             for instrument in open_trades['symbol']:
